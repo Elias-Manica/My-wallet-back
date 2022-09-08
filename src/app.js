2371c554e07de378e9fe64fed1579de19494567c
elias <eliasmanica@poli.ufrj.br>
2022-09-08 17:25:12 -0300

    login e sing-up com token

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import dotenv from "dotenv";
 import joi from "joi";
 import dayjs from "dayjs";
 
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+
 dotenv.config();
 
 const app = express();
@@ -26,6 +29,115 @@ const entraceSchema = joi.object({
     .empty("")
     .regex(/[a-zA-Z0-9]/),
 });
+
+const singupSchema = joi.object({
+  name: joi
+    .string()
+    .required()
+    .empty("")
+    .regex(/[a-zA-Z0-9]/),
+  email: joi.string().required().empty("").email(),
+  password: joi
+    .string()
+    .required()
+    .empty("")
+    .regex(/[a-zA-Z0-9]/),
+});
+
+const loginSchema = joi.object({
+  email: joi.string().required().empty("").email(),
+  password: joi
+    .string()
+    .required()
+    .empty("")
+    .regex(/[a-zA-Z0-9]/),
+});
+
+//Cadastro
+app.post("/sing-up", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const validation = singupSchema.validate(req.body, { abortEarly: false });
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    res.status(422).send(errors);
+    return;
+  }
+
+  const passwordHash = bcrypt.hashSync(password, 10);
+
+  try {
+    const findUser = await db.collection("users").findOne({ email });
+    if (findUser) {
+      res.status(401).send({ message: "Email já cadastrado" });
+      return;
+    } else {
+      await db
+        .collection("users")
+        .insertOne({ name, email, password: passwordHash });
+      return res.status(201).send({ message: "Email cadastrado com sucesso" });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//Login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const validation = loginSchema.validate(req.body, { abortEarly: false });
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    res.status(422).send(errors);
+    return;
+  }
+
+  try {
+    const findUser = await db.collection("users").findOne({ email });
+
+    if (findUser && bcrypt.compareSync(password, findUser.password)) {
+      const token = uuid();
+
+      const findSession = await db.collection("sessions").findOne({
+        userId: findUser._id,
+      });
+
+      if (findSession) {
+        const response = await db.collection("sessions").findOne({
+          userId: findUser._id,
+        });
+        console.log(response);
+        const update = await db.collection("sessions").updateOne(
+          {
+            userId: findUser._id,
+          },
+          { $set: { token: token } }
+        );
+        console.log(update);
+        res.status(200).send(token);
+        return;
+      } else {
+        await db.collection("sessions").insertOne({
+          userId: findUser._id,
+          token,
+        });
+        res.status(200).send(token);
+        return;
+      }
+    } else {
+      res.status(404).send({ message: "Email ou Senha incorretos" });
+      return;
+    }
+  } catch (error) {
+    res.status(500).send(error);
+    return;
+  }
+});
+
+app.post("/transition", async (req, res) => {});
 
 //Entrada
 app.post("/deposity", async (req, res) => {
@@ -163,11 +275,5 @@ app.get("/balance", async (req, res) => {
     res.sendStatus(500);
   }
 });
-
-//Cadastro
-app.post("/sing-up", async (req, res) => {});
-
-//Login
-app.post("/sing-in", async (req, res) => {});
 
 app.listen(5000, () => console.log("Server listen on port 5000"));
